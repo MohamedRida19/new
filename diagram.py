bbd3888fea407c36b6604af8489927cfdb5a2d82
mohamed Rida <rayanereda90@gmail.com>
2023-06-09 09:31:10 +0000

    adding subplots into the diagrams

--- diagram.py
+++ diagram.py
@@ -3,8 +3,17 @@ import matplotlib.pyplot as plt
 
 x = np.array([1,2,4,7])
 y = np.array([1,4,2,7])
-
+plt.title('1')
+plt.subplot(1,2,1)
 plt.plot(x,y)
+
+x = np.array([1,5,9,10])
+y = np.array([1,6,4,14])
+plt.subplot(1,2,2)
+plt.title('2')
+plt.plot(x,y)
+
+
 plt.show()
 
 
