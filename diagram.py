f70c1af4394c99d384383086d09132b2830912b0
mohamed Rida <rayanereda90@gmail.com>
2023-06-09 11:11:24 +0000

    deleting titles

--- diagram.py
+++ diagram.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 x= np.array([1,2,3,5])
 y = np.array([1,3,2,6])
-plt.title('1')
+
 plt.subplot(1,2,1)
 plt.plot(x,y)
 
 x = np.array([1,2,3,4])
 y = np.array([1,5,2,17])
-plt.title('2')
+
 plt.subplot(1,2,2)
 plt.plot(x,y)
 
