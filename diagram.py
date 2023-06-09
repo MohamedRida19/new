05d7982b29b066f7c1cf1f1632a7ac08a2c6d8f0
mohamed Rida <rayanereda90@gmail.com>
2023-06-09 09:46:50 +0000

    adding titles

--- diagram.py
+++ diagram.py
@@ -1,24 +1,17 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-x = np.array([1,2,4,7])
-y = np.array([1,4,2,7])
+x= np.array([1,2,3,5])
+y = np.array([1,3,2,6])
 plt.title('1')
 plt.subplot(1,2,1)
 plt.plot(x,y)
 
-x = np.array([1,5,9,10])
-y = np.array([1,6,4,14])
-plt.subplot(1,2,2)
+x = np.array([1,2,3,4])
+y = np.array([1,5,2,17])
 plt.title('2')
+plt.subplot(1,2,2)
 plt.plot(x,y)
 
-
+plt.suptitle("TWIX")
 plt.show()
-
-
-
-
-
-
-
